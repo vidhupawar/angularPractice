9d63b16c3a8658fa9338bfc5a144a984bdc9d355
vidhupawar <vidhupawar@gmail.com>
2016-06-28 08:27:03 +0530

    task manager

---
+++ TaskManagerApp/js/task.js
@@ -0,0 +1,32 @@
+var taskApp = angular.module("taskApp",["utils"]);
+taskApp.controller("taskController",function($scope,Task){
+$scope.taskObject={
+    taskList:[],
+    searchOn:false,
+
+    addNewTask:function(){
+        var newTask = new Task($scope.taskName,$scope.taskDesc);
+        console.log('Task is:',newTask);
+        this.taskList.push(newTask);
+    },
+    countCompletedTask:function(){
+
+        return this.taskList.filter(function(task){
+            return task.isCompleted;
+        }).length;
+    },
+    removeCompletedTask:function(){
+        this.taskList=this.taskList.filter(function(task){
+            return !task.isCompleted;
+        });
+    },
+    saveCompletedTask:function(){
+
+    },
+    countPending:function(){
+        return this.taskList.filter(function(task){
+            return !task.isCompleted;
+        }).length;
+    }
+}
+});
